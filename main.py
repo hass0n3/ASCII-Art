966b1f61303d822c8dc3458f6fddd81b551efac1
hassen <>
2021-05-21 17:03:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Question 1")
+print("{\__/}")
+print("( •.•)")
+print("[ >< ]")
+
+print("Question 2")
+print("(=^_^=)")
+
+print("Question 3")
+print("(-.-)Zz")
+
+print("Question 4")
+
+print("Question 5")
+print("(\_/)")
+print("(^ ^)")
+print("( - )")
+print("( - )")
+print(" \_/")
